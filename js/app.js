db438ba819358e6847dce78f8c3a8f7b562d13cf
benjamingleitz <gleitz.benjamin@gmail.com>
2023-05-08 14:21:15 +0200

    modif

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-
 // On se crée un "tableau associatif" des entêtes pour les lignes et colonnes
+// => pour y accéder : gridHeaders.columns ou gridHeaders['rows']
 const gridHeaders = {
     // index  0  1  2  3  4  5  6  7
     'rows' : [1, 2, 3, 4, 5, 6, 7, 8],
@@ -10,37 +10,44 @@ const gridHeaders = {
 
 // On modélise une ligne de bataille navale à l'aide d'un tableau. Chaque entrée vide représente une cellule et chaque entrée "b" représente une cellule contenant un bateau.
 let grid = [
-    ['', '', 'b', 'b', 'b', '', '', '',], // 0
+    ['', '', 't', 't', 't', '', '', '',], // 0
     ['', '', '', '', '', '', '', '',], // 1
-    ['', '', 'b', '', '', '', '', '',], // 2
-    ['', '', 'b', '', '', 'b', 'b', '',], // 3
+    ['', '', 'p', 'p', '', 'b', 'b', '',], // 2
+    ['p', '', '', '', '', '', '', '',], // 3
     ['', '', 'b', '', '', '', '', '',], // 4
-    ['', '', '', '', '', '', '', '',], // 5
-    ['', '', '', 'b', 'b', '', '', '',], // 6
-    ['', '', '', '', '', 'b', 'b', '',], // 7
+    ['', '', 'b', '', '', '', 'p', '',], // 5
+    ['', '', 'b', '', 'p', '', '', '',], // 6
+    ['', '', '', '', '', '', '', '',], // 7
 ];
+
+// console.log(grid);
 
 // Affichage de la grille
 displayGrid();
 
+
+
 function displayGrid() {
 
-    // On commence par afficher les entetes de colonnes.
+    // On commence par afficher les entetes de colonnes. C'est pas super pratique car si change le nombre de colonnes, on doit venir modifier le nombre d'entetes.
+    // console.log('  A B C D E F G H');
     console.log('  ' + gridHeaders.columns.join(' '));
+    // On utilise plutot notre tableau gridHeaders qui sert de source pour nos entetes
+
 
     // On parcourt chacune des lignes du tableau "grid"
     for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
         // Chaque ligne est récupérée dans le constante row
         const row = grid[rowIndex];
         // Comme on a déjà fait une fonction qui se charge d'afficher les infos d'une ligne, on peut l'appeler en lui passant la ligne courante.
-        
         // On récupère la chaine de caractères représentant la ligne.
+        
         let stringLine = displayLine(row, rowIndex);
 
         // Pour afficher les entetes de lignes, on récupère l'index courant auquel on ajoute 1 !
         const lineNumber = gridHeaders.rows[rowIndex];
 
-        // On affiche la ligne 
+        // On affiche la ligne !
         console.log(lineNumber + ' ' + stringLine);
 
 
@@ -58,6 +65,7 @@ function displayLine(gridLine, rowIndex) {
 
     for (let columnIndex = 0; columnIndex < gridLine.length; columnIndex++) {
         
+        // Grace à columnIndex, on peut récupérer chacune des entrées du tableau gridLine.
         const currentCharacter = gridLine[columnIndex];
 
         // On analyse maintenant le contenu de currentCharacter. Si c'est une une chaine vide, alors on doit ajouter un ~ dans la variable stringLine.
@@ -68,6 +76,7 @@ function displayLine(gridLine, rowIndex) {
             // Sinon, c'est que c'est un autre caractère, on le concatène à notre variable stringLine. 
             stringLine += currentCharacter;
 
+            // On veut afficher dans la grille HTML les lettres correspondantes à notre tableau JS. Si j'ai un B dans le tableau, je dois avoir un b dans la grille HTML.
             // On crée un sélecteur CSS pour sélectionner la cellule. Chaque cellule a un ID qui commence par "cell" suivi de l'index de ligne et de l'index de colonne.
             const currentHtmlCell = document.querySelector('#cell' + rowIndex + columnIndex);
             // On définit le contenu texte de la cellule HTML comme étant le caractère courant.
@@ -82,6 +91,7 @@ function displayLine(gridLine, rowIndex) {
                 currentHtmlCell.classList.add('hit');
             }
         }
+        // Ensuite, on ajoute un espace pour aérer entre les caractères
         stringLine += ' ';
         
     }
@@ -104,6 +114,7 @@ function sendMissileAt(rowIndex, columnIndex) {
 
     if(targetCell === "b"){
         console.log("Touché !");
+        alert('Touché !');
 
         // Mettre à jour la grille pour ajouter un t à la place du b
         grid[rowIndex][columnIndex] = 't';
@@ -122,6 +133,9 @@ function sendMissileAt(rowIndex, columnIndex) {
     }
     else {
         console.log("Plouf !");
+        alert('Raté, essaie encore !');
+
+
         // Mettre à jour la grille pour ajouter un p dans la cellule
         grid[rowIndex][columnIndex] = 'p';
 
@@ -152,6 +166,7 @@ function sendMissile(cellName) {
 // Fonction dont le but est renvoyer un tableau contenant les index correspondant aux coordonnées passées en paramètre : A5 doit donner row: 4 et column: 0
 function getGridIndexes(cellName) {
 
+    // 1- On décortique la chaine de caractères. C'est à dire, on récupère d'un coté la lettre et d'un coté le chiffre
     const cellLetter = cellName[0];
     let cellNumber = cellName[1];
  
@@ -211,6 +226,7 @@ function checkGameOver() {
 // Fonction demandant à l'utilisateur de taper des coordonnées de cellule à viser, à travers un prompt.
 function promptMissileCell() {
 
+    // La fonction prompt permet d'afficher une fenetre popup et l'utilisateur entrée du texte dedans. Ce texte sera récupéré dans la variable targetedCell.
     let targetedCell = prompt('Où envoyer le prochain missile ?');
 
     // On envoie le nom de la cellule récupérée à la fonction sendMissile qui va donc envoyer un missile dessus ! 
@@ -225,9 +241,10 @@ function displayHits() {
     // On affiche la longueur du tableau hitCells grace à la propriété length
     console.log("Nombre de cellules touchées : " + hitCells.length);
 
-    // On parcourt le tableau hitCells et on affiche l'id de chaque cellule grace à la propriété "id"
+    // On parcourt le tableau hitCells et on affiche l'attribut de donnée "cell-name" de chaque cellule grace à la propriété "dataset"
     for (const cell of hitCells) {
-        console.log(cell.id);
+        // Les "dataset" (attributs de données) sont obligatoirement écrits en kebabCase dans l'HTML. Pour les utiliser en JS, il faut les convertir en camelCase. Donc "cell-name" devient "cellName".
+        console.log(cell.dataset.cellName);
     }
 
 }
